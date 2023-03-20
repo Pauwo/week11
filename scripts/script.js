e95d9a3324689c35382a076a122cc79e8bf10adf
Paulo <youremail@yourdomain.com>
2023-03-20 11:23:10 -0700

    javascript practice

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,85 @@
-function greet (name) {
-    console.log(`Hello ${name}`)
+// function declaration
+// function greet (name) {
+//     console.log(`Hello ${name}`)
+// }
+// // function calling
+// greet("Paulo")
+
+// function expression
+// const greet = function (name) {
+//     console.log(`Hello ${name}`);
+// }
+// //function calling
+// greet ("Paulo")
+
+// // function expression
+// const greet = (name) => {
+//     console.log(`Hello ${name}`)
+// }
+// // function calling
+// greet()
+
+// // function declaration
+// function greet (name, time = "Day") {
+//     console.log(`Good ${time} ${name}`)
+// }
+// // function calling
+// greet("Paulo")
+
+
+// const value = prompt("Enter a value: ");
+
+// // references to p tags
+// const radiusPara = document.querySelector("#radius");
+// radiusPara.textContent = `Value entered by user is ${value}`
+// console.log(radiusPara);
+// const resultPara = document.querySelector("#result");
+// console.log(resultPara);
+
+// function calculateArea(radius) {
+//     if(isNaN(radius)) {
+//         // error: this is not a number
+//         resultPara.textContent = "Error: This is not a number";
+//     } else {
+//     const area = Math.PI * radius * radius;
+//     return area.toFixed(2);
+//     }
+// }
+
+// let result = calculateArea(value);
+// console.log(result)        
+// alert(`The area of the circle with the ${value} is ${result}`);
+// resultPara.textContent =`The area of the circle with the ${value} is ${result}`;
+
+
+function populatelist(myShoppingList)
+{
+    // get access to the ul element
+    const ulElement = document.querySelector("ul.shopping");
+    // console.log(ulElement)
+    // loop through myShoppingList array
+    for (let item of myShoppingList) {
+        // console.log(item)
+        let newli = document.createElement("li")
+        newli.textContent = item;
+        ulElement.appendChild(newli);
+
+    // add an li under the ul element with text equal to each item of my 
+    }
 }
 
-greet("Paulo")
+let shoppingList = ['bread', 'cheese', 'green pepper'];
+populatelist(shoppingList);
+
+
+function squarelist() {
+    // get access to the ul
+    const elem = document.querySelector("ul.circlelist");
+    // remove circlelist class
+    elem.classList.remove("circlelist");
+    // add squarelist
+    elem.classList.add("squarelist");
+}
+
+
+squarelist();
